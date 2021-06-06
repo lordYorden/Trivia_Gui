56472be939fedf0adde5724b82fb599738f286e3
yarden perets <yordenperets@gmail.com>
2021-06-06 22:18:50 +0300

    added shuffled questions and display correct answer func

--- TriviaGUI/GameScreen.xaml.cs
+++ TriviaGUI/GameScreen.xaml.cs
@@ -23,12 +23,19 @@ namespace TriviaGUI
         private RoomData _metadata;
         private QuestionData _questionData;
         private Communicator _coms;
+        private List<Button> _bAnswers;
+        private static Random rng = new Random();
         public GameScreen(RoomData metadata, Communicator communicator)
         {
             InitializeComponent();
             _coms = communicator;
             _currQuestionCount = 1;
             _metadata = metadata;
+            _bAnswers = new List<Button>();
+            _bAnswers.Add(LAnswerRed);
+            _bAnswers.Add(LAnswerGreen);
+            _bAnswers.Add(LAnswerYellow);
+            _bAnswers.Add(LAnswerBlue);
 
             List<string> a = new List<string>();
             a.Add("Golda Meir");
@@ -44,19 +51,53 @@ namespace TriviaGUI
 
         private void updateQuestion()
         {
+            Random rnd = new Random();
             LQuestionCount.Text = "Question " + _currQuestionCount;
             _currQuestionCount++;
             LQuestion.Text = _questionData.Question;
-            LAnswer2.Content = _questionData.CorrectAnswer;
-            LAnswer3.Content = _questionData.OtherAnswers[0];
-            LAnswer4.Content = _questionData.OtherAnswers[1];
-            LAnswer1.Content = _questionData.OtherAnswers[2];
+            List<string> questions = new List<string>(_questionData.OtherAnswers);
+            questions.Add(_questionData.CorrectAnswer);
+            questions = questions.OrderBy(a => rng.Next()).ToList();
+            for (int i = 0; i < _bAnswers.Count; i++)
+            {
+                _bAnswers[i].Content = questions[i];
+            }
             LTimer.Text = _metadata.TimePerQuestion.ToString();
+        }
+
+        private void displayCorrectAnswer() 
+        {
+            string bName = "";
+            Style bStyle = null;
+            for (int i = 0; i < _bAnswers.Count; i++)
+            {
+                if (_bAnswers[i].Content != _questionData.CorrectAnswer)
+                {
+                    bName = _bAnswers[i].Name;
+                    if (bName == "LAnswerRed")
+                        bStyle = this.FindResource("RedWorngButtonStyle") as Style;
+                    else if (bName == "LAnswerGreen")
+                        bStyle = this.FindResource("GreenWorngButtonStyle") as Style;
+                    else if (bName == "LAnswerBlue")
+                        bStyle = this.FindResource("BlueWorngButtonStyle") as Style;
+                    else
+                        bStyle = this.FindResource("YellowWorngButtonStyle") as Style;
+                    _bAnswers[i].Style = bStyle;
+                }
+                _bAnswers[i].IsHitTestVisible = false;
+
+            }
         }
 
         private void GameScreen_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
             _coms.leaveGameRequest();
+        }
+
+        private void LAnswerRed_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("hI");
+            displayCorrectAnswer();
         }
     }
 }
